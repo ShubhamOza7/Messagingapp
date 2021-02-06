9f2dfa9cba52479d2b71bf5f4bb0032587e1378a
ShubhamOza7 <30323786+ShubhamOza7@users.noreply.github.com>
2021-02-06 13:01:56 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,91 @@
+import React, {useEffect, useLayoutEffect, useState} from 'react'
+import { TouchableOpacity } from 'react-native'
+import {  SafeAreaView ,StyleSheet,ScrollView, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
+import CustomListItem from '../components/CustomListItem'
+import { auth, db } from '../firebase'
+import {AntDesign, SimpleLineIcons} from "@expo/vector-icons"
+import { StatusBar } from 'react-native'
+
+const HomeScreen = ({navigation}) => {
+    const[chats,setChats] = useState ([])
+    const signOutUser = () => {
+        auth.signOut().then(() => {
+            navigation.replace('Login')
+        })
+    }
+    useEffect(() => {
+        const unsubscribe = db .collection('chats').onSnapshot(snapshot => (
+            setChats(snapshot.docs.map(doc =>({
+                id: doc.id,    
+                data: doc.data()
+            })))
+        ) 
+    )
+    return unsubscribe;
+},[])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title:"Ciao",
+            headerStyle:{backgroundColor: "#fffc00"},
+            headerTitleStyle: { color: "black"}, 
+            headerTitleAlign:"Center",
+            headerTintColor:"white",
+            headerLeft: () => (
+                        <View style = {{marginLeft: 20}}>
+                    <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+                        <Avatar rounded source={{uri : auth?.currentUser?.photoURL}}/>
+                    </TouchableOpacity>
+                        </View>
+            ),
+
+            headerRight: () => (
+                    <View style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width:80,
+                        marginRight:20,
+                    }}>
+                        <TouchableOpacity activeOpacity={0.5}> 
+                            <AntDesign name="camerao" size={24} color="black"/>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                        onPress={()=>navigation.navigate("AddChat")}
+                        activeOpacity={0.5}>
+                            <SimpleLineIcons name="pencil" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
+            )
+        });
+        
+        
+    }, [navigation])
+    
+const enterChat = (id,chatName) => {
+    navigation.navigate('Chat',{
+        id,
+        chatName,
+    })
+}
+
+    return (
+        <SafeAreaView>
+         
+            <ScrollView>
+            <StatusBar style="light"/>
+            {chats.map(({id,data: {chatName}}) => (
+                <CustomListItem key={id} id={id} chatName={chatName} enterChat={enterChat}/>
+            ))}
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+ 
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container:{
+        height:"100%"
+    }
+})
